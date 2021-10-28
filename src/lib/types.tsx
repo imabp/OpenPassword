4181d7199ca660f3c1e22bd058c5124eda2c8052
Abir <abir.p@affinidi.com>
2021-10-28 10:44:48 +0530

    update types file

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -1,23 +1,38 @@
 export type user = {
     id: string;
-    personals: user_personals
-    email: string
-    vaults: vault[]
+    personals: user_personals;
+    email: string;
+    vaults: vault[];
 
 }
 export type user_personals = {
-    first_name: string,
-    last_name: string,
-    github: string,
+    first_name: string;
+    last_name: string;
+    github: string;
+    avatar?:string;
 
 }
 export type vault = {
-    id: string,
-    members: user[],
-    store:secretType[]
+    id: string;
+    memberIDs: string[];
+    store:secretType[];
 }
 
 export type secretType = {
     key: string,
     value: string,
+}
+
+export type newUserReqType={
+    first_name:string,
+    last_name:string,
+    email:string,
+    github:string,
+}
+
+export type Error ={
+    code:number,
+    name:string,
+    description?:string,
+    resolution?:string,
 }
